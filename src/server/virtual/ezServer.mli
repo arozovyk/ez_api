bd19e792914b1e30dbe096637107bcce3d354f02
arozovyk <artemiy.rozovyk@ocamlpro.com>
2022-02-25 12:24:53 +0100

    fix mli

--- src/server/virtual/ezServer.mli
+++ src/server/virtual/ezServer.mli
@@ -1,4 +1,4 @@
-val server : ?catch:(string -> exn -> string EzAPIServerUtils.Answer.t Lwt.t) ->
+val server : ?geoip: 'a option -> ?catch:(string -> exn -> string EzAPIServerUtils.Answer.t Lwt.t) ->
   (int * EzAPIServerUtils.server_kind) list -> unit Lwt.t
 
 val set_debug : unit -> unit
